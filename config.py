682b5411a6bf8ea32dd71ab9beb1bac288f54bc9
alijon-nd <alijon.student@wust.edu>
2026-06-23 19:03:33 -0700

    Update config.py

--- config.py
+++ config.py
@@ -11,6 +11,8 @@ GCS_FOLDER_PATH = os.getenv("GCS_FOLDER_PATH", "ansocial1/")
 DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
 DEEPSEEK_URL = "https://api.deepseek.com/v1/chat/completions"
 
+OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+
 # Meta API (Facebook & Instagram)
 FACEBOOK_PAGE_ID = os.getenv("FACEBOOK_PAGE_ID")
 FACEBOOK_ACCESS_TOKEN = os.getenv("FACEBOOK_ACCESS_TOKEN")
