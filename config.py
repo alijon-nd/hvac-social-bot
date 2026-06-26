bb28c03433362422df90c112e167a4ad5b12704f
alijon-nd <alijon.student@wust.edu>
2026-06-26 06:08:18 -0700

    Update config.py

--- config.py
+++ config.py
@@ -7,8 +7,8 @@ load_dotenv()
 GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME", "ansocial")
 GCS_FOLDER_PATH = os.getenv("GCS_FOLDER_PATH", "ansocial1/")
 
-# OpenRouter (free)
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+# DeepSeek API
+DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
 
 # Meta (later)
 FACEBOOK_PAGE_ID = os.getenv("FACEBOOK_PAGE_ID")
